c8be5cf2422ed426fa1ce6f1bf896e17d9fe235c
JuanManuelDeluca1 <101626028+JuanManuelDeluca1@users.noreply.github.com>
2022-06-11 15:06:51 -0300

    Add files via upload

---
+++ tp3_windows/parser.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Passenger.h"
+
+/** \brief Parsea los datos los datos de los pasajeros desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListPassenger LinkedList*
+ * \return int
+ *
+ */
+int parser_PassengerFromText(FILE* pFile , LinkedList* pArrayListPassenger)
+{
+	Passenger* p;
+	if(pArrayListPassenger != NULL)
+	{
+	  while( !feof(pFile))
+	  {
+		  fscanf("%d, [^,],[^,],%.2f,%d,[^,]\n", p->id, p->nombre, p->apellido, p->precio, p->tipoPasajero, p->codigoVuelo);
+	  }
+	}
+    return 1;
+}
+/** \brief Parsea los datos los datos de los pasajeros desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListPassenger LinkedList*
+ * \return int
+ *
+ */
+int parser_PassengerFromBinary(FILE* pFile , LinkedList* pArrayListPassenger)
+{
+	Passenger auxPassenger;
+	fread(&auxPassenger, sizeof(Passenger), 1 ,pFile);
+    return 1;
+}
